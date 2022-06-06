fe01f9c5dd8744f83bcd1fd60d4ae7954ec298ec
Jessy Damasceno <jessy_bas@hotmail.com>
2022-06-06 16:39:22 -0300

    add req 2 - teste de About

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,21 @@
-test('começando testes', () => {});
+import React from 'react';
+import { queryByText, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/renderWithRouter';
+import About from '../pages/About';
+
+describe('Teste o componente <About.js />', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const POKEDEX_SOURCE = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const aboutText = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(aboutText).toBeInTheDocument();
+
+    const pokedexImg = screen.getByAltText('Pokédex');
+    expect(pokedexImg).toBeInTheDocument();
+    expect(pokedexImg).toHaveProperty('src', POKEDEX_SOURCE);
+
+    const moreDetails = screen.queryByText('More details');
+    expect(moreDetails).not.toBeInTheDocument();
+  });
+});
