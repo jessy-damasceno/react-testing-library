d9c4b784f5ce6478f07a5b403e5ca5d3a7cd4d02
Jessy Damasceno <jessy_bas@hotmail.com>
2022-06-06 16:54:26 -0300

    req 3 - teste componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Teste o componente FavoritePokemons', () => {
+  it('Teste se é exibida mensagem caso a pessoa não tenha pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavoFoundText = screen.getByText('No favorite pokemon found');
+
+    expect(noFavoFoundText).toBeInTheDocument();
+  });
+});
