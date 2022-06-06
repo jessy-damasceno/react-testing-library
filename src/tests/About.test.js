afa1dee37868b899eeca0a378d9b07e1e5cbb3d2
Jessy Damasceno <jessy_bas@hotmail.com>
2022-06-06 16:40:03 -0300

    fix lint req 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { queryByText, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import renderWithRouter from '../helpers/renderWithRouter';
 import About from '../pages/About';
 
