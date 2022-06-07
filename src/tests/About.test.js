a6be7e0029a7ded501f6dda04663a0a4eea0bba3
Jessy Damasceno <jessy_bas@hotmail.com>
2022-06-07 14:26:59 -0300

    add req6

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -4,7 +4,7 @@ import renderWithRouter from '../helpers/renderWithRouter';
 import About from '../pages/About';
 
 describe('Teste o componente <About.js />', () => {
-  it('Teste se a página contém as informações sobre a Pokédex', () => {
+  it.only('Teste se a página contém as informações sobre a Pokédex', () => {
     renderWithRouter(<About />);
     const POKEDEX_SOURCE = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
     const aboutText = screen.getByRole('heading', { name: 'About Pokédex' });
@@ -16,5 +16,8 @@ describe('Teste o componente <About.js />', () => {
 
     const moreDetails = screen.queryByText('More details');
     expect(moreDetails).not.toBeInTheDocument();
+
+    const paragraphList = document.getElementsByTagName('p');
+    expect(paragraphList).toHaveLength(2);
   });
 });
